b91763b07cd08ee78addb45ea9089115546283c8
chenzhen10 <teekeen32@gmail.com>
2019-01-08 22:47:30 +0300

    s

--- task-master/jwd-task01-template/src/main/java/by/tc/task01/dao/ApplianceDAO.java
+++
@@ -1,8 +0,0 @@
-package by.tc.task01.dao;
-
-import by.tc.task01.entity.Appliance;
-import by.tc.task01.entity.criteria.Criteria;
-
-public interface ApplianceDAO {
-	<E> Appliance find(Criteria<E> criteria);
-}
--- task-master/jwd-task01-template/src/main/java/by/tc/task01/entity/Appliance.java
+++
@@ -1,9 +0,0 @@
-package by.tc.task01.entity;
-
-public abstract class Appliance {
-    public abstract void setFields(String fieldsLine);
-    public void print() {
-        System.out.println(toString());
-    }
-
-}
